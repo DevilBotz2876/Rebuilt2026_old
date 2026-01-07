8235685a3b27baf91d313d0cfd4376eaf70ca2a0
Ethan Porter <emanporter2009@gmail.com>
2026-01-07 09:19:18 -0500

    camera returns latest result

--- src/main/java/frc/robot/subsystems/implementations/vision/camera/CameraPhoton.java
+++ src/main/java/frc/robot/subsystems/implementations/vision/camera/CameraPhoton.java
@@ -30,96 +30,97 @@ public class CameraPhoton implements Camera {
   @Override
   public void updateInputs(CameraInputs inputs) {
     inputs.isConnected = camera.isConnected();
-    // inputs.fps = unsure right now
-
     List<PhotonPipelineResult> results = camera.getAllUnreadResults();
-    poseMeasurements = new VisionPoseMeasurement[results.size()];
 
-    for (int i = 0; i < results.size(); i++) {
-      PhotonPipelineResult result = results.get(i);
-      VisionPoseMeasurement measurement = new VisionPoseMeasurement();
-
-      if (!result.hasTargets()) {
-        // no tags
-        measurement.targetIds = new int[0];
-        inputs.targetIds = new int[0];
-
-      } else if (result.multitagResult.isPresent()) {
-        // 2+ tags
-        MultiTargetPNPResult multitagResult = result.multitagResult.get();
-
-        Pose2d cameraPose =
-            new Pose3d(Translation3d.kZero, Rotation3d.kZero)
-                .plus(multitagResult.estimatedPose.best)
-                .toPose2d();
-        inputs.cameraPose = cameraPose;
-
-        inputs.targetIds = new int[multitagResult.fiducialIDsUsed.size()];
-        for (int j = 0; j < inputs.targetIds.length; j++) {
-          inputs.targetIds[j] = multitagResult.fiducialIDsUsed.get(j).intValue();
-        }
-
-        measurement.robotPose =
-            cameraPose.transformBy(
-                (new Transform2d(
-                        robotToCamera.getTranslation().toTranslation2d(),
-                        robotToCamera.getRotation().toRotation2d()))
-                    .inverse());
-        measurement.targetIds = inputs.targetIds;
-        measurement.ambiguity = multitagResult.estimatedPose.ambiguity;
-        measurement.timestamp = result.getTimestampSeconds();
-        // robot to camera + camera to target = robot to target
-        measurement.robotToBestTargetDistanceInMeters =
-            robotToCamera
-                .plus(result.getBestTarget().getBestCameraToTarget())
-                .getTranslation()
-                .getDistance(Translation3d.kZero);
-
-        inputs.cameraDistanceToTargetMeters =
-            result
-                .getBestTarget()
-                .getBestCameraToTarget()
-                .getTranslation()
-                .getDistance(Translation3d.kZero);
-      } else {
-        // one tag
-        // use location on field to determine pose
-
-        Pose3d aprilTagPose = tagLayout.getTagPose(result.getBestTarget().fiducialId).get();
-        Pose2d cameraPose =
-            new Pose3d(aprilTagPose.getTranslation(), aprilTagPose.getRotation())
-                .plus(result.getBestTarget().bestCameraToTarget.inverse())
-                .toPose2d();
-        inputs.cameraPose = cameraPose;
-        inputs.targetIds = new int[1];
-        inputs.targetIds[0] = result.getBestTarget().fiducialId;
-
-        measurement.robotPose =
-            cameraPose.transformBy(
-                (new Transform2d(
-                        robotToCamera.getTranslation().toTranslation2d(),
-                        robotToCamera.getRotation().toRotation2d()))
-                    .inverse());
-        measurement.targetIds = inputs.targetIds;
-        measurement.ambiguity = result.getBestTarget().getPoseAmbiguity();
-        measurement.timestamp = result.getTimestampSeconds();
-
-        // robot to camera + camera to target = robot to target
-        measurement.robotToBestTargetDistanceInMeters =
-            robotToCamera
-                .plus(result.getBestTarget().getBestCameraToTarget())
-                .getTranslation()
-                .getDistance(Translation3d.kZero);
-
-        inputs.cameraDistanceToTargetMeters =
-            result
-                .getBestTarget()
-                .getBestCameraToTarget()
-                .getTranslation()
-                .getDistance(Translation3d.kZero);
-      }
-      poseMeasurements[i] = measurement;
+    if (results.isEmpty()) {
+      this.poseMeasurements = new VisionPoseMeasurement[0];
+      return;
     }
+
+    PhotonPipelineResult result = results.get(results.size() - 1); // get latest result
+    this.poseMeasurements = new VisionPoseMeasurement[1];
+    VisionPoseMeasurement measurement = new VisionPoseMeasurement();
+
+    if (!result.hasTargets()) {
+      // no tags
+      measurement.targetIds = new int[0];
+      inputs.targetIds = new int[0];
+
+    } else if (result.multitagResult.isPresent()) {
+      // 2+ tags
+      MultiTargetPNPResult multitagResult = result.multitagResult.get();
+
+      Pose2d cameraPose =
+          new Pose3d(Translation3d.kZero, Rotation3d.kZero)
+              .plus(multitagResult.estimatedPose.best)
+              .toPose2d();
+      inputs.cameraPose = cameraPose;
+
+      inputs.targetIds = new int[multitagResult.fiducialIDsUsed.size()];
+      for (int j = 0; j < inputs.targetIds.length; j++) {
+        inputs.targetIds[j] = multitagResult.fiducialIDsUsed.get(j).intValue();
+      }
+
+      measurement.robotPose =
+          cameraPose.transformBy(
+              (new Transform2d(
+                      robotToCamera.getTranslation().toTranslation2d(),
+                      robotToCamera.getRotation().toRotation2d()))
+                  .inverse());
+      measurement.targetIds = inputs.targetIds;
+      measurement.ambiguity = multitagResult.estimatedPose.ambiguity;
+      measurement.timestamp = result.getTimestampSeconds();
+      // robot to camera + camera to target = robot to target
+      measurement.robotToBestTargetDistanceInMeters =
+          robotToCamera
+              .plus(result.getBestTarget().getBestCameraToTarget())
+              .getTranslation()
+              .getDistance(Translation3d.kZero);
+
+      inputs.cameraDistanceToTargetMeters =
+          result
+              .getBestTarget()
+              .getBestCameraToTarget()
+              .getTranslation()
+              .getDistance(Translation3d.kZero);
+    } else {
+      // one tag
+      // use location on field to determine pose
+
+      Pose3d aprilTagPose = tagLayout.getTagPose(result.getBestTarget().fiducialId).get();
+      Pose2d cameraPose =
+          new Pose3d(aprilTagPose.getTranslation(), aprilTagPose.getRotation())
+              .plus(result.getBestTarget().bestCameraToTarget.inverse())
+              .toPose2d();
+      inputs.cameraPose = cameraPose;
+      inputs.targetIds = new int[1];
+      inputs.targetIds[0] = result.getBestTarget().fiducialId;
+
+      measurement.robotPose =
+          cameraPose.transformBy(
+              (new Transform2d(
+                      robotToCamera.getTranslation().toTranslation2d(),
+                      robotToCamera.getRotation().toRotation2d()))
+                  .inverse());
+      measurement.targetIds = inputs.targetIds;
+      measurement.ambiguity = result.getBestTarget().getPoseAmbiguity();
+      measurement.timestamp = result.getTimestampSeconds();
+
+      // robot to camera + camera to target = robot to target
+      measurement.robotToBestTargetDistanceInMeters =
+          robotToCamera
+              .plus(result.getBestTarget().getBestCameraToTarget())
+              .getTranslation()
+              .getDistance(Translation3d.kZero);
+
+      inputs.cameraDistanceToTargetMeters =
+          result
+              .getBestTarget()
+              .getBestCameraToTarget()
+              .getTranslation()
+              .getDistance(Translation3d.kZero);
+    }
+    poseMeasurements[0] = measurement;
   }
 
   @Override
